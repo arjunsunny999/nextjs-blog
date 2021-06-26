8b6bed735999a981ab319c6d1f43f353839dd3ee
arjunsunny999 <31948646+arjunsunny999@users.noreply.github.com>
2021-06-26 13:28:40 +0530

    Add files via upload
    layout update

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Arjun Sunny'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Arjun Sunny '
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Career information page"
         />
         <meta
           property="og:image"
